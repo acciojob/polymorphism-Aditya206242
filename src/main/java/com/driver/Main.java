d4083e5796c51ffdc4e35aaedaa2b7cd01081cf5
Aditya kumar <90274804+Aditya206242@users.noreply.github.com>
2025-09-14 09:06:55 +0530

    Assignment 1

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,30 @@ package com.driver;
 
 public class Main {
 
+  public class product{
+
+    public int product(int a,int b){
+      return a+b;
+    }
+
+    public int product(int a,int b,int c){
+      return a+b+c;
+    }
+
+    public double product(int a,int b,int c){
+      return a+b+c;
+    }
+  }
+
+  public static void main(String[] args ){
+
+    product p =new product();
+    System.out.println(p.product(5,10));
+    System.out.println(p.product(5,10,15));
+    System.out.println(p.product(5.1,10.1,15.1));
+    
+    
+    
+  }
+
 }
